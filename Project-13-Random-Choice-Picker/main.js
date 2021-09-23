18e29b2ce97ed2cdb99bd2ee674a4f5adc4076e5
Susanto <nerdyspook007@gmail.com>
2021-09-23 22:55:13 +0530

    feat: add random choice selection

--- Project-13-Random-Choice-Picker/main.js
+++ Project-13-Random-Choice-Picker/main.js
@@ -3,8 +3,56 @@ const textArea = document.querySelector("#textarea");
 
 textArea.focus(); // If we open the page it'll will automatically put the cursor in the textarea
 
+function randomSelect() {
+    const times = 30;
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag();
+
+        highlightTag(randomTag);
+
+        setTimeout(() => {
+            unHighlightTag(randomTag);
+        }, 100);
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag();
+
+            highlightTag(randomTag);
+        }, 100);
+    }, times * 100);
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll(".tag");
+    return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+    tag.classList.add("highlight");
+}
+
+function unHighlightTag(tag) {
+    tag.classList.remove("highlight");
+}
+
 textArea.addEventListener("keyup", (element) => {
     createTags(element.target.value);
+
+    if(element.key === "Enter") {
+
+        // Wait 10 miliseconds
+        setTimeout(() => {
+            element.target.value = "";
+
+        }, 10);
+
+        randomSelect();
+    }
 })
 
 function createTags(input) {
